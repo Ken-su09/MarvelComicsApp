152bf4a15463db7ba58f6d60d104b1ddf81c73c5
Ken Suon <ken.su_dev@hotmail.com>
2021-11-08 00:47:56 +0100

    Dependency Injection Setup

---
+++ app/src/main/java/com/suonk/marvelcomicsapp/di/App.kt
@@ -0,0 +1,8 @@
+package com.suonk.marvelcomicsapp.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
---
+++ app/src/main/java/com/suonk/marvelcomicsapp/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.suonk.marvelcomicsapp.di
+
+import com.suonk.marvelcomicsapp.api.MarvelApiService
+import com.suonk.marvelcomicsapp.api.MarvelApiService.Companion.BASE_URL
+import com.suonk.marvelcomicsapp.repositories.DefaultRepository
+import com.suonk.marvelcomicsapp.repositories.MainRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): MarvelApiService =
+        retrofit.create(MarvelApiService::class.java)
+
+    @Provides
+    fun provideDefaultRepository(api: MarvelApiService): DefaultRepository =
+        MainRepository(api)
+}
