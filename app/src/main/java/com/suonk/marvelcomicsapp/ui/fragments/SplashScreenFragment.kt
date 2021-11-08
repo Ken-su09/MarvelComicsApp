cc65a065971cfae4fba02820e47f43dfa47c090b
Ken Suon <ken.su_dev@hotmail.com>
2021-11-08 01:28:48 +0100

    SplashScreen Done

--- app/src/main/java/com/suonk/marvelcomicsapp/ui/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/suonk/marvelcomicsapp/ui/fragments/SplashScreenFragment.kt
@@ -6,16 +6,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import com.suonk.marvelcomicsapp.R
+import com.suonk.marvelcomicsapp.databinding.FragmentSplashScreenBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
-class SplashScreenFragment : Fragment() {
+class SplashScreenFragment : BaseFragment() {
+
+    private var binding: FragmentSplashScreenBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            appLogoAnimation()
+            delay(1200)
+            val navDir =
+                SplashScreenFragmentDirections.actionSplashScreenFragmentToAllComicsFragment()
+            navController.navigate(navDir)
+        }
+    }
+
+    private fun appLogoAnimation() {
+        val animation = AnimationUtils.loadAnimation(context, R.anim.suddenly_appear)
+        binding?.appName?.startAnimation(animation)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
